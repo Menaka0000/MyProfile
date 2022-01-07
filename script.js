a55cae2165995197d6f2842ef8b9d7eafc2436fd
Menaka0000 <m.lakshan0000@gmali.com>
2022-01-07 13:45:18 +0530

    some animations was added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+/*
+var animation=bodymovin.loadAnimation({
+    container:document.getElementById('anim'),
+    render:'svg',
+    loop:false,
+    autoplay:true,
+    display:false,
+    path:'data.json'
+})*/
